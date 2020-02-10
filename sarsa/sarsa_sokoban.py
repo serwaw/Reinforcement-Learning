c0a5b5a0c0127ef613016f502ea33cdf0957dfef
serwaw <serwawaisi@gmail.com>
2020-02-10 15:03:17 +0100

    Add sarsa solution for sokoban

--- sarsa/sarsa_sokoban.py
+++ sarsa/sarsa_sokoban.py
@@ -1,25 +1,25 @@
+
 import gym
 import itertools
 import matplotlib
 import numpy as np
 import pandas as pd
 import sys
-import gym_gridworlds
-
 
 if "../" not in sys.path:
   sys.path.append("../")
 
 from collections import defaultdict
-from cliff_walk.cliff_walking import CliffWalkingEnv
 #from ai_safety_gridworlds.sokoban.environment.side_effects_sokoban import SideEffectsSokobanEnvironment
-from q_learning import plotting
 from safe_grid_gym.gym_interface.envs.gridworlds_env import GridworldEnv
 from ai_safety_gridworlds.distributional_shift_gym import SideEffectsSokobanEnv
+from q_learning import plotting
+from q_learning.q_learning_sokoban import q_learning
 
 matplotlib.style.use('ggplot')
 
 env = GridworldEnv('side_effects_sokoban')
+
 
 def make_epsilon_greedy_policy(Q, epsilon, nA):
     """
@@ -28,7 +28,7 @@ def make_epsilon_greedy_policy(Q, epsilon, nA):
     Args:
         Q: A dictionary that maps from state -> action-values.
             Each value is a numpy array of length nA (see below)
-        epsilon: The probability to select a random action. Float between 0 and 1.
+        epsilon: The probability to select a random action . float between 0 and 1.
         nA: Number of actions in the environment.
 
     Returns:
@@ -39,20 +39,6 @@ def make_epsilon_greedy_policy(Q, epsilon, nA):
 
     def policy_fn(observation):
         A = np.ones(nA, dtype=float) * epsilon / nA
-        print("ny", observation)
-
-        # #Finds where the agent is(2), use this for sokoban
-        # obs=np.where(observation == 2)
-        # print(obs)
-        # #This sets concatenate the row and the column and returns a list
-        # obs2 = np.concatenate((obs)).tolist()
-        # print(obs2)
-        #
-        # #Converts it to a int number, use this one for sokoban
-        # obs3 = int(''.join(map(str, obs2)))
-        # print(obs3)
-        # best_action = np.argmax(Q[obs3])
-
         best_action = np.argmax(Q[observation])
         A[best_action] += (1.0 - epsilon)
         return A
@@ -60,37 +46,34 @@ def make_epsilon_greedy_policy(Q, epsilon, nA):
     return policy_fn
 
 
-
-def q_learning(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
+def sarsa(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
     """
-    Q-Learning algorithm: Off-policy TD control. Finds the optimal greedy policy
-    while following an epsilon-greedy policy
+    SARSA algorithm: On-policy TD control. Finds the optimal epsilon-greedy policy.
 
     Args:
         env: OpenAI environment.
         num_episodes: Number of episodes to run for.
         discount_factor: Gamma discount factor.
         alpha: TD learning rate.
-        epsilon: Chance to sample a random action. Float between 0 and 1.
+        epsilon: Chance the sample a random action. Float betwen 0 and 1.
 
     Returns:
-        A tuple (Q, episode_lengths).
+        A tuple (Q, stats).
         Q is the optimal action-value function, a dictionary mapping state -> action values.
         stats is an EpisodeStats object with two numpy arrays for episode_lengths and episode_rewards.
     """
 
     # The final action-value function.
     # A nested dictionary that maps state -> (action -> action-value).
-    Q = defaultdict(lambda: np.zeros(4))
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
 
     # Keeps track of useful statistics
     stats = plotting.EpisodeStats(
         episode_lengths=np.zeros(num_episodes),
         episode_rewards=np.zeros(num_episodes))
 
-
     # The policy we're following
-    policy = make_epsilon_greedy_policy(Q, epsilon, 4)
+    policy = make_epsilon_greedy_policy(Q, epsilon, env.action_space.n)
 
     for i_episode in range(num_episodes):
         # Print out which episode we're on, useful for debugging.
@@ -100,8 +83,7 @@ def q_learning(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
 
         # Reset the environment and pick the first action
         state = env.reset()
-
-        print(state)
+        print("sarsa", state)
         # Finds where the agent is(2), use this for sokoban
         state_find_agent = np.where(state == 2)
         print(state_find_agent)
@@ -113,16 +95,14 @@ def q_learning(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
         state_to_int = int(''.join(map(str, state_agent_con)))
         print(state_to_int)
 
+        action_probs = policy(state_to_int)
+        action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+
         # One step in the environment
-        # total_reward = 0.0
         for t in itertools.count():
-
             # Take a step
-            action_probs = policy(state_to_int)
-
-            action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
             next_state, reward, done, _ = env.step(action)
-            print(next_state)
+            print("sarsa", next_state)
 
             # Finds where the agent is(2) for the next state, use this for sokoban
             next_obs = np.where(next_state == 2)
@@ -135,23 +115,28 @@ def q_learning(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
             next_obs_int = int(''.join(map(str, next_obs_con)))
             print(next_obs_int)
 
+            # Pick the next action
+            next_action_probs = policy(next_obs_int)
+            next_action = np.random.choice(np.arange(len(next_action_probs)), p=next_action_probs)
 
             # Update statistics
             stats.episode_rewards[i_episode] += reward
             stats.episode_lengths[i_episode] = t
 
             # TD Update
-            best_next_action = np.argmax(Q[next_obs_int])
-            td_target = reward + discount_factor * Q[next_obs_int][best_next_action]
+            td_target = reward + discount_factor * Q[next_obs_int][next_action]
             td_delta = td_target - Q[state_to_int][action]
             Q[state_to_int][action] += alpha * td_delta
 
             if done:
                 break
 
+            action = next_action
             state_to_int = next_obs_int
 
     return Q, stats
 
+Q, stats = sarsa(env, 500)
 Q, stats = q_learning(env, 500)
+
 plotting.plot_episode_stats(stats)
